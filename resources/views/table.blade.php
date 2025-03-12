b8106f25ba5ad519d59ddd2d22d89abbe249d577
Meaownx <saedelta16alifah@gmail.com>
2025-03-12 14:47:10 +0700

    menambahkan modal point

---
+++ resources/views/table.blade.php
@@ -0,0 +1,44 @@
+@extends('layout.template')
+
+@section('content')
+<table class="table table-striped">
+    <head>
+        <tr>
+            <th>No</th>
+            <th>Nama</th>
+            <th>NIM</th>
+            <th>Kelas</th>
+        </tr>
+        <tr>
+            <td>1</td>
+            <td>Alifah</td>
+            <td>23/515178/SV/22490</td>
+            <td>A</td>
+        </tr>
+        <tr>
+            <td>2</td>
+            <td>Cece</td>
+            <td>23/512298/SV/22217</td>
+            <td>A</td>
+        </tr>
+        <tr>
+            <td>3</td>
+            <td>Myla</td>
+            <td>23/522046/SV/23577</td>
+            <td>B</td>
+        </tr>
+        <tr>
+            <td>4</td>
+            <td>Zahra</td>
+            <td>23/518296/SV/22946</td>
+            <td>A</td>
+        </tr>
+        <tr>
+            <td>5</td>
+            <td>Angel</td>
+            <td>23/514461/SV/22354</td>
+            <td>B</td>
+        </tr>
+    </head>
+</table>
+@endsection
